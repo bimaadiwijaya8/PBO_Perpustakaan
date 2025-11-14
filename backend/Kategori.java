90e350a60680d748399145367072752e32f52de4
Bima Adiwijaya <bimaadiwijaya83@gmail.com>
2025-11-14 09:29:01 +0700

    Pecobaan 3.6

--- backend/Kategori.java
+++ backend/Kategori.java
@@ -2,79 +2,58 @@ package backend;
 
 import java.util.ArrayList;
 import java.sql.*;
-
 public class Kategori {
-    // 3. Atribut sesuai field pada tabel kategori
+
+    // 3. Atribut
     private int idkategori;
     private String nama;
     private String keterangan;
 
-    // 5. Konstruktor default
+    // 5. Konstruktor
     public Kategori() {
     }
 
-    // 5. Konstruktor custom
     public Kategori(String nama, String keterangan) {
         this.nama = nama;
         this.keterangan = keterangan;
     }
 
-    // 4. Getter
+    // 4. Getter dan Setter
     public int getIdkategori() {
         return idkategori;
+    }
+
+    public void setIdkategori(int idkategori) {
+        this.idkategori = idkategori;
     }
 
     public String getNama() {
         return nama;
     }
 
-    public String getKeterangan() {
-        return keterangan;
-    }
-
-    // 4. Setter
-    public void setIdkategori(int idkategori) {
-        this.idkategori = idkategori;
-    }
-
     public void setNama(String nama) {
         this.nama = nama;
+    }
+
+    public String getKeterangan() {
+        return keterangan;
     }
 
     public void setKeterangan(String keterangan) {
         this.keterangan = keterangan;
     }
 
-    // Override toString() agar ComboBox menampilkan nama kategori (dari Percobaan 8)
-    @Override
-    public String toString() {
-        return nama;
-    }
-
-    // --- Method Helper (Private) ---
-
-    // Mengambil objek Kategori dari ResultSet (digunakan di getById, getAll, search)
-    private Kategori setKategori(ResultSet rs) {
-        Kategori kat = new Kategori();
-        try {
-            kat.setIdkategori(rs.getInt("idkategori"));
-            kat.setNama(rs.getString("nama"));
-            kat.setKeterangan(rs.getString("keterangan"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return kat;
-    }
-
-    // --- Method CRUD ---
-
     // 6. Method getById()
     public Kategori getById(int id) {
         Kategori kat = new Kategori();
-        ResultSet rs = DBHelper.selectQuery("SELECT * FROM kategori WHERE idkategori = '" + id + "'");
+        ResultSet rs = DBHelper.selectQuery("SELECT * FROM kategori "
+                                         + " WHERE idkategori = '" + id + "'");
         try {
             while (rs.next()) {
-                kat = setKategori(rs);
+                kat = new Kategori();
+                kat.setIdkategori(rs.getInt("idkategori"));
+                kat.setNama(rs.getString("nama"));
+                kat.setKeterangan(rs.getString("keterangan"));
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -85,12 +64,13 @@ public class Kategori {
     // 7. Method getAll()
     public ArrayList<Kategori> getAll() {
         ArrayList<Kategori> ListKategori = new ArrayList();
-        String sql = "SELECT * FROM kategori";
-        ResultSet rs = DBHelper.selectQuery(sql);
-
+        ResultSet rs = DBHelper.selectQuery("SELECT * FROM kategori");
         try {
             while (rs.next()) {
-                Kategori kat = setKategori(rs);
+                Kategori kat = new Kategori();
+                kat.setIdkategori(rs.getInt("idkategori"));
+                kat.setNama(rs.getString("nama"));
+                kat.setKeterangan(rs.getString("keterangan"));
                 ListKategori.add(kat);
             }
         } catch (Exception e) {
@@ -103,13 +83,15 @@ public class Kategori {
     public ArrayList<Kategori> search(String keyword) {
         ArrayList<Kategori> ListKategori = new ArrayList();
         String sql = "SELECT * FROM kategori WHERE "
-                + " nama LIKE '%" + keyword + "%' "
-                + " OR keterangan LIKE '%" + keyword + "%' ";
+                   + "     nama LIKE '%" + keyword + "%' "
+                   + "     OR keterangan LIKE '%" + keyword + "%' ";
         ResultSet rs = DBHelper.selectQuery(sql);
-
         try {
             while (rs.next()) {
-                Kategori kat = setKategori(rs);
+                Kategori kat = new Kategori();
+                kat.setIdkategori(rs.getInt("idkategori"));
+                kat.setNama(rs.getString("nama"));
+                kat.setKeterangan(rs.getString("keterangan"));
                 ListKategori.add(kat);
             }
         } catch (Exception e) {
@@ -118,21 +100,19 @@ public class Kategori {
         return ListKategori;
     }
 
-    // 9. Method save() (Insert/Update)
+    // 9. Method save()
     public void save() {
         if (getById(idkategori).getIdkategori() == 0) {
-            // INSERT
             String SQL = "INSERT INTO kategori (nama, keterangan) VALUES("
-                    + " '" + this.nama + "', "
-                    + " '" + this.keterangan + "' "
-                    + " )";
+                       + "     '" + this.nama + "', "
+                       + "     '" + this.keterangan + "' "
+                       + "     )";
             this.idkategori = DBHelper.insertQueryGetId(SQL);
         } else {
-            // UPDATE
             String SQL = "UPDATE kategori SET "
-                    + " nama = '" + this.nama + "', "
-                    + " keterangan = '" + this.keterangan + "' "
-                    + " WHERE idkategori = '" + this.idkategori + "'";
+                       + "     nama = '" + this.nama + "', "
+                       + "     keterangan = '" + this.keterangan + "' "
+                       + "     WHERE idkategori = '" + this.idkategori + "'";
             DBHelper.executeQuery(SQL);
         }
     }
